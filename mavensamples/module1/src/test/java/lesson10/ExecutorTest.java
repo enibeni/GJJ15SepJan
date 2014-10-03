93e85e212fa2e0a990abbf2cf3a7ae19a0517507
Alexandr Kiyko <alexander.vmk@gmail.com>
2014-10-03 23:53:57 +0400

    Lesson10 samples

---
+++ mavensamples/module1/src/test/java/lesson10/ExecutorTest.java
@@ -0,0 +1,41 @@
+package lesson10;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Alex
+ * @since 03.10.14
+ */
+public class ExecutorTest {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
